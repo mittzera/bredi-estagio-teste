e889f34e6adfd8e52cdce5627bb046378bed7ba7
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2024-12-11 01:29:33 -0300

    feat: button turned to a link, redirecting to the main website

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function Button() {
   return (
-    <div className="flex justify-center items-center h-full my-10">
-      <div className="border-black border-2 text-black bg-[#F7D622] w-fit p-3 px-14 text-xl font-bold">
-        Veja Mais
-      </div>
+    <div className="flex justify-center items-center h-full my-[8rem]">
+      <a href="https://www.bredi.com.br/" target="_blank">
+        <div className="border-black border-2 text-black bg-[#F7D622] w-fit p-3 px-14 text-xl font-bold">
+          Veja Mais
+        </div>
+      </a>
     </div>
   );
 }
