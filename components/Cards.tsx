7fb3efbb81ee6bcf319df680df165abfaa76a2ce
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2024-12-11 01:30:12 -0300

    style: cards styling update

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -4,13 +4,22 @@ import React from "react";
 
 const Cards: React.FC = () => {
   return (
-    <div className=" mt-10 flex flex-col sm:flex-row md:flex-row lg:flex-row gap-5 justify-center" id="cards">
+    <div
+      className=" mt-10 flex flex-col sm:flex-row md:flex-row lg:flex-row gap-5 justify-center "
+      id="cards"
+    >
       {cards.map((card, index) => (
         <div
           key={index}
-          className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-5 flex flex-col items-center md:items-start"
+          className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-5 flex flex-col items-center md:items-start "
         >
-          <Image src={card.imagem} alt={card.titulo} width={300} height={300} />
+          <Image
+            className="rounded-lg"
+            src={card.imagem}
+            alt={card.titulo}
+            width={300}
+            height={300}
+          />
           <h1 className="mt-2 font-bold text-black text-xl text-left">
             {card.titulo}
           </h1>
